71f97b85d708750857585c7b64d75f8626fe3f64
CastleD <castle3ddd@gmail.com>
2025-02-18 18:34:28 -0600

    Todo App Update

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,90 +1,91 @@
 class FormValidator {
-    constructor(settings, formElement) {
-        this._inputSelector = settings.inputSelector;
-        this._submitButtonSelector = settings.submitButtonSelector
-        this._errorClass = settings.errorClass
-        this._inputErrorClass = settings.inputErrorClass
-        this._inactiveButtonClass = settings.inactiveButtonClass
-        this._formElement = formElement
+  constructor(settings, formElement) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formElement = formElement;
+  }
+
+  _checkInputValidity(inputElement) {
+    console.log(inputElement);
+    const formEl = this._formElement;
+    const settings = this._settings;
+
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
     }
+  }
 
-        _checkInputValidity(inputElement) {
-            console.log(inputElement);
-            const formEl = this._formElement;
-            const settings = this._settings;
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => !inputElement.validity.valid);
+  }
 
-            if (!inputElement.validity.valid) {
-                this._showInputError(inputElement, inputElement.validationMessage);
-              } else {
-                this._hideInputError( inputElement );
-              }
-        }
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`
+    );
 
-        _hasInvalidInput() {
-            return this._inputList.some((inputElement) => !inputElement.validity.valid);
-        }
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
 
-        _showInputError(inputElement, errorMessage) {
-            const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`); 
-        
-            inputElement.classList.add(this._inputErrorClass);
-            errorElement.textContent = errorMessage;
-            errorElement.classList.add(this._errorClass);
-        }
+  _hideInputError(inputElement) {
+    console.log(`#{inputElement.id}-error`);
+    const errorElement = document.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.textContent = "";
+    errorElement.classList.remove(this._errorClass);
+  }
 
-        _hideInputError(inputElement) {
-            console.log(`#{inputElement.id}-error`);
-            const errorElement = document.querySelector(`#${inputElement.id}-error`);
-            inputElement.classList.remove(this._inputErrorClass);
-            errorElement.textContent = "";
-            errorElement.classList.remove(this._errorClass);
-        }
-
-        _toggleButtonState() {
-            if(this._hasInvalidInput()) {
-                this._buttonElement.classList.add(this._inactiveButtonClass);
-                this._buttonElement.disabled = true;
-            } else {
-                this._buttonElement.classList.remove(this._inactiveButtonClass);
-                this._buttonElement.disabled = false;
-            }
-        }
-
-    _setEventListeners() {
-        this._inputList = Array.from(
-            this._formElement.querySelectorAll(this._inputSelector),
-          );
-
-
-        this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
-        
-        this._toggleButtonState();
-        
-          this._inputList.forEach((inputElement) => {
-            inputElement.addEventListener("input", () => {
-                this._checkInputValidity(inputElement);
-                this._toggleButtonState(this._buttonElement);
-            });
-          });
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
     }
+  }
 
-    resetValidation() {
-        this._inputList.forEach((inputElement) => {
-            inputElement.value = "";
-            this._hideInputError(inputElement);
-        });
-        this._toggleButtonState();
-    }
-        
-      
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._inputSelector)
+    );
 
-    enableValidation() {
-        this._formElement.addEventListener("submit", (evt) => {
-          evt.preventDefault();
-        });
-        this._setEventListeners();
-        this._toggleButtonState();
-    }
+    this._buttonElement = this._formElement.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState(this._buttonElement);
+      });
+    });
+  }
+
+  resetValidation() {
+    this._inputList.forEach((inputElement) => {
+      inputElement.value = "";
+      this._hideInputError(inputElement);
+    });
+    this._toggleButtonState();
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+    this._toggleButtonState();
+  }
 }
 
 export default FormValidator;
--- components/Todo.js
+++ components/Todo.js
@@ -1,61 +1,57 @@
 class Todo {
-    constructor (data, selector) {
-        this._data = data;
-        this._templateElement = document.querySelector(selector);
-    }
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
 
-    _setEventListeners() {
-        this._todoDeleteBtn.addEventListener("click", () => {
-            this._todoElement.remove();
-            });
+  _setEventListeners() {
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
 
-        this._todoCheckboxEl.addEventListener("change", () => {
-            this._data.completed = !this._data.completed;
-            console.log(this._data.completed);
-        })
-    }
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+      console.log(this._data.completed);
+    });
+  }
 
-    _generateCheckboxEl() {
-        this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-        this._todoLabel = this._todoElement.querySelector(".todo__label");
-        this._todoCheckboxEl.checked = this._data.completed;
-        this._todoCheckboxEl.id = `todo-${this._data.id}`;
-        this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
-    }
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
 
-    _generateDueDate() {
-        this.dueDate = new Date(this._data.date);
-        this.todoDate = this._todoElement.querySelector(".todo__date");
-         if (!isNaN(this.dueDate)) {
-        this.todoDate.textContent = `Due: ${this.dueDate.toLocaleString("en-US", {
+  _generateDueDate() {
+    this.dueDate = new Date(this._data.date);
+    this.todoDate = this._todoElement.querySelector(".todo__date");
+    if (!isNaN(this.dueDate)) {
+      this.todoDate.textContent = `Due: ${this.dueDate.toLocaleString("en-US", {
         year: "numeric",
         month: "short",
         day: "numeric",
-       }
-    )}`;
+      })}`;
     }
-}
+  }
 
-    getView() {
+  getView() {
     this._todoElement = this._templateElement.content
-     .querySelector(".todo")
-     .cloneNode(true);
+      .querySelector(".todo")
+      .cloneNode(true);
 
     const todoNameEl = this._todoElement.querySelector(".todo__name");
-    
+
     this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
     todoNameEl.textContent = this._data.name;
 
-
     this._generateCheckboxEl();
     this._setEventListeners();
     this._generateDueDate();
-    
+
     return this._todoElement;
-
-
-    }
+  }
 }
 
 export default Todo;
